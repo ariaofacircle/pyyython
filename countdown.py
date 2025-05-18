f8ef8c0e51e82bbf98bb8dba383f125ec031384e
ariaofacircle <apbtld@gmail.com>
2025-05-18 14:10:33 +0800

    Create countdown.py
    countdown to happy new year kay why not ( learned about the time library )

---
+++ countdown.py
@@ -0,0 +1,12 @@
+# countdown program
+import time
+
+for x in reversed(range(1,11)):
+    print(x)
+    time.sleep(1)
+
+print("HAPPY NEW YEAR !!")
+
+# improvement plans
+# font ng text tapos interface siguro ??
+# DAPAT MALAKI TALAGA FR
